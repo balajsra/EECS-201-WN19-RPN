2d70833c190d240a006a39515cad117ebb488b14
Sravan Balaji <balajsra@umich.edu>
2019-03-01 10:30:02 -0500

    Failing test cases
    Added tests for basic math operations (+, -, *, /)

--- test_rpn.py
+++ test_rpn.py
@@ -2,6 +2,34 @@ import unittest
 import rpn
 
 class TestBasics(unittest.TestCase):
-    def test_add(self):
+    def test_add_1(self):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2, result)
+
+    def test_add_2(self):
+        result = rpn.calculate('5 2 +')
+        self.assertEqual(7, result)
+
+    def test_sub_1(self):
+        result = rpn.calculate('10 5 -')
+        self.assertEqual(5, result)
+
+    def test_sub_2(self):
+        result = rpn.calculate('22 13 -')
+        self.assertEqual(9, result)
+
+    def test_mul_1(self):
+        result = rpn.calculate('3 5 *')
+        self.assertEqual(15, result)
+
+    def test_mul_2(self):
+        result = rpn.calculate('2 3 *')
+        self.assertEqual(6, result)
+
+    def test_div_1(self):
+        result = rpn.caclulate('28 7 /')
+        self.assertEqual(4, result)
+
+    def test_div_2(self):
+        result = rpn.calculate('50 5 /')
+        self.assertEqual(10, result)
